f16b17af59a741d5e897c6ae2b70685e62ad8d53
shivanipolichetti <shivanipolichetti2@gmail.com>
2024-04-12 15:09:35 +0530

    crud operations

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ButtonComponent from './classcomponents/button';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonComponent/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/classcomponents/button.jsx
@@ -0,0 +1,142 @@
+import { Component } from "react";
+import "./style.css";
+
+class ButtonComponent extends Component{
+    state={
+        isSubscribe:false,
+        text1:"Subscribed",
+        text2:"Subscribe",
+        cards:[ {
+            "id": 1,
+            "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+            "price": 109.95,
+            "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            "category": "men's clothing",
+            "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+            "rating": 4.7
+        },
+        {
+            "id": 2,
+            "title": "Mens Casual Premium Slim Fit T-Shirts ",
+            "price": 22.3,
+            "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+            "category": "men's clothing",
+            "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+            "rating":4.9
+        },
+        {
+            "id": 3,
+            "title": "Mens Cotton Jacket",
+            "price": 55.99,
+            "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+            "category": "men's clothing",
+            "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+            "rating": 5
+        },
+        {
+            "id": 4,
+            "title": "Mens Casual Slim Fit",
+            "price": 15.99,
+            "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+            "category": "men's clothing",
+            "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            "rating": 4.0
+        },
+        {
+            "id": 5,
+            "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+            "price": 695,
+            "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+            "category": "jewelery",
+            "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+            "rating": 4.5
+        }]
+       
+    }
+
+    changeSubscribe=()=>{
+        this.setState(
+            {
+                isSubscribe:!this.state.isSubscribe
+            }
+        )
+    }
+    addCard=()=>{
+        const newCard=[...this.state.cards,"newCard"]
+        //console.log(newCard);
+       this.setState(
+        {
+            cards:newCard
+        }
+       )
+    }
+    
+deleteCard=(index)=>{
+    const newCard=this.state.cards.filter((eachCard,i)=>i!==index)
+    //console.log("Deleting card at index:", index);
+    this.setState(
+        {
+            cards:newCard
+        }
+    )
+
+}
+updateCard=(index)=>{
+    const newCard=this.state.cards.map((eachCard,i)=>{
+        if(i===index){
+            return {
+               ...eachCard,
+               title:"leather jacket",
+               category:"men-clothing",
+               price:13.99,
+               rating:4.2,
+               image:"https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+            };
+        }
+        else{
+            return eachCard
+        }
+        
+    })
+    this.setState(
+        {
+            cards:newCard
+        }
+    )
+    console.log(newCard)
+}
+          
+    render(){
+        return(
+            <div> 
+           <button onClick={this.changeSubscribe}>{this.state.isSubscribe?this.state.text1:this.state.text2}</button>{
+            this.state.isSubscribe?
+            <>
+            <h1>Welcome to Shopping Page</h1>
+            {this.state.cards.map((card,index)=>(
+                <div className="card-container">
+                <div key={card.id} className="card-form">
+                    <img src={card.image} alt={card.title} width={100}></img>
+                    <h1>{card.title}</h1>
+                    <h1>{card.category}</h1>
+                    <h1>{card.price}</h1>
+                    <h1>{card.rating}</h1>
+                    <button onClick={this.addCard}>Add</button>
+                    <button onClick={()=>this.updateCard(index)}>Update</button>
+                    <button onClick={()=>this.deleteCard(index)}>Delete</button>
+                </div>
+                </div>
+               
+            ))}
+            </>
+            :
+            <h1>Please Subscribe to Shop</h1>
+           }
+        </div>
+          
+            
+        )
+    }
+}
+
+export default ButtonComponent;
